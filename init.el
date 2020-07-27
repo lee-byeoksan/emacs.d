02b8101d587f38b7e374cb5b7e3a85e05574c8d3
Lee Byeoksan <lee.byeoksan+github@gmail.com>
2020-07-28 02:34:39 +0900

    Add user-package and vterm

--- init.el
+++ init.el
@@ -5,6 +5,10 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
 ;; General Settings
 (menu-bar-mode 0)
 (if window-system
@@ -31,9 +35,19 @@
 
 ;;; Added by Custom
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (misterioso)))
+ '(package-selected-packages (quote (use-package)))
  '(winner-mode t))
-(custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; Personal Settings
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
--- lisp/init-terminal.el
+++ lisp/init-terminal.el
@@ -1,8 +1,20 @@
 ;; -*- mode: elisp -*-
 
-(add-hook 'term-mode-hook
-          (lambda ()
-            (display-line-numbers-mode 0)
-            (setq-local global-hl-line-mode nil)))
+(defun common-term-setup ()
+  (display-line-numbers-mode 0)
+  (setq-local global-hl-line-mode nil))
+
+;; term, ansi-term
+(add-hook 'term-mode-hook #'common-term-setup)
+
+;; vterm
+(use-package vterm
+  :ensure t
+  :bind
+  ("C-]" . vterm--self-insert)
+  ("C-u" . vterm--self-insert)
+  :config
+  (setq vterm-always-compile-module t)
+  :hook (vterm-mode . common-term-setup))
 
 (provide 'init-terminal)
