fe20d564440de4e449de7ca71dfcf253ce0e311f
Lee Byeoksan <lee.byeoksan+github@gmail.com>
2020-08-10 20:39:03 +0900

    org: prepare GTD

--- init.el
+++ init.el
@@ -57,12 +57,7 @@
  '(custom-enabled-themes (quote (misterioso)))
  '(package-selected-packages (quote (use-package)))
  '(winner-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
 
 ;; Personal Settings
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -4,8 +4,25 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c c") 'org-capture)
 
+(setq org-directory "~/.orgs")
+(defvar byeoksan/org-inbox (concat org-directory "/inbox.org"))
+
 (setq org-indent-mode t)
 (setq org-special-ctrl-a/e t)
-(setq org-directory "~/.orgs")
+(setq org-default-notes-file byeoksan/org-inbox)
+(setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "WAIT(w)" "SCHED(s)" "DONE(d)")))
+(setq org-capture-templates
+      '(("t" "TODO" entry
+         (file+headline "" "Tasks")
+         (file "templates/todo.tpl")
+         :kill-buffer t)
+        ("n" "Fleeting note" entry
+         (file+headline "" "Notes")
+         (file "templates/flt-note.tpl")
+         :kill-buffer t)
+        ))
+(setq org-agenda-window-setup 'only-window)
+(setq org-agenda-restore-windows-after-quit t)
+(setq org-agenda-files (list byeoksan/org-inbox))
 
 (provide 'init-org)
