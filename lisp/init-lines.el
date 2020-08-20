5b207e51bb9d8470f202b79e79ce3ace463b869b
Lee Byeoksan <lee.byeoksan+github@gmail.com>
2020-08-20 21:13:23 +0900

    lines: remove free variable

--- lisp/init-lines.el
+++ lisp/init-lines.el
@@ -1,15 +1,12 @@
 ;; -*- lexical-binding: t -*-
 
-(setq mode-hooks
-      '(text-mode-hook
-        prog-mode-hook
-        org-mode-hook
-        ))
-
 (defun enable-line-numbers ()
   (display-line-numbers-mode 1))
 
-(dolist (hook mode-hooks)
-        (add-hook hook 'enable-line-numbers))
+(let ((mode-hooks '(text-mode-hook
+                    prog-mode-hook
+                    org-mode-hook)))
+  (dolist (hook mode-hooks)
+    (add-hook hook 'enable-line-numbers)))
 
 (provide 'init-lines)
