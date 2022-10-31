08d645f4ee3b6e3de6d2758e3a58256f30936326
Lee Byeoksan <lee.byeoksan@MacbookAir.local>
2022-10-31 14:43:17 +0900

    Setup tide

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -73,11 +73,70 @@
   :ensure t)
 
 ;; Typescript
-(add-hook 'typescript-mode 'eglot-ensure)
-(use-package web-mode
-  :ensure t
-  :mode "\\.tsx\\'")
+;; (add-hook 'typescript-mode 'eglot-ensure)
+;; Use tide instead of eglot, eglot + typescript-language-server is way slow
 
+;; https://github.com/katspaugh/.emacs.d/blob/master/packages.el
+(use-package typescript-mode
+  :mode (("\\.ts\\'" . typescript-mode))
+  :mode (("\\.tsx\\'" . typescript-mode)))
+
+;; (defun setup-tide-mode ()
+;;   (interactive)
+;;   (defun tide-imenu-index () nil)
+;;   (tide-setup)
+;;   (tide-hl-identifier-mode +1))
+
+;; (use-package tide
+;;   :ensure t
+;;   :after (typescript-mode company flycheck)
+;;   :hook ((typescript-mode . #'setup-tide-mode)
+;;          (js-mode . #'setup-tide-mode)
+;;          (js2-mode . #'setup-tide-mode)
+;;          (rjsx-mode . #'setup-tide-mode)))
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         ;; (before-save . tide-format-before-save)
+         ))
+
+;; (use-package mmm-mode
+;;   :config
+;;   (progn
+;;     (setq mmm-global-mode t)
+;;     (setq mmm-submode-decoration-level 0) ;; Turn off background highlight
+;;     (mmm-add-classes
+;;      '((mmm-styled-mode
+;;         :submode css-mode
+;;         :front "\\(styled\\|css\\)[.()<>[:alnum:]]?+`"
+;;         :back "`;")))
+;;     (mmm-add-mode-ext-class 'typescript-mode nil 'mmm-styled-mode)
+
+;;     (mmm-add-classes
+
+;;      '((mmm-jsx-mode
+;;         :front "\\(return\s\\|n\s\\|(\n\s*\\)<"
+;;         :front-offset -1
+;;         :back ">\n?\s*)"
+;;         :back-offset 1
+;;         :submode web-mode)))
+;;     (mmm-add-mode-ext-class 'typescript-mode nil 'mmm-jsx-mode)))
+
+;; (defun mmm-reapply ()
+;;   (mmm-mode)
+;;   (mmm-mode))
+
+;; (add-hook 'after-save-hook
+;;           (lambda ()
+;;             (when (string-match-p "\\.tsx?" buffer-file-name)
+;;               (mmm-reapply))))
+
+;; (use-package web-mode
+;;   :ensure t
+;;   :mode "\\.tsx\\'")
 
 ;; Protobuf
 (use-package protobuf-mode
